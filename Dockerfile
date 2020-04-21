6c83a232ad93d9001514a13ccfe571d060e91055
Robbie Van Gorkom <robbie.van.gorkom@protonmail.com>
2020-04-20 22:22:57 -0700

    Absracted out monitor and rclone parts.

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
-FROM alpine:3.7
+FROM vangorra/rclone-monitor-docker:latest
 
-RUN apk --no-cache add samba bash inotify-tools supervisor && \
-	apk --no-cache add --virtual=build-dependencies wget curl unzip ca-certificates && \
-	wget -o rclone.zip https://downloads.rclone.org/v1.40/rclone-v1.40-linux-386.zip && \
-	unzip rclone-v1.40-linux-386.zip && \
-	cp rclone-v1.40-linux-386/rclone /usr/local/bin/rclone && \
-	rm -rf rclone-v1.40-linux-386 && \
-	rm rclone-v1.40-linux-386.zip && \
-	apk del --purge build-dependencies
+RUN apk --no-cache add samba supervisor
 
 COPY config-base/* /config-base/
 COPY bin/* /usr/local/bin/
+COPY etc /etc
 
 # exposes samba's default ports (137, 138 for nmbd and 139, 445 for smbd)
 EXPOSE 137/udp 138/udp 139 445
--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-RCLONE_CONFIG_FILE="/config/rclone.conf"
-SETUP_COMPLETE_FILE="/samba-rclone-issetupcomplete"
 DEFAULT_SHARE_USERNAME="files"
 DEFAULT_SHARE_PASSWORD="FrogsHouseLight"
 DEFAULT_SHARE_NAME="files"
@@ -76,72 +74,45 @@ SHARE_USERNAME=$(coalesce "$SHARE_USERNAME" "$DEFAULT_SHARE_USERNAME")
 SHARE_PASSWORD=$(coalesce "$SHARE_PASSWORD" "$DEFAULT_SHARE_PASSWORD")
 SHARE_NAME=$(coalesce "$SHARE_NAME" "$DEFAULT_SHARE_NAME")
 HOSTNAME=$(coalesce "$HOSTNAME" "$DEFAULT_HOSTNAME")
-
-if ! [[ -e "$RCLONE_CONFIG_FILE" ]]; then
-  errorMsg "'$RCLONE_CONFIG_FILE' does not exist. You need to map it with --volume <hostpath to config dir>:/config. Or you need to configure rclone."
-  exit 1
-fi
-
-if [[ -z "$RCLONE_DESTINATION" ]]; then
-  errorMsg "Destination was not provided."
-  showUsage
-  exit 1
-fi
-
-# Verify config section in rclone.conf exists for destination provided.
-RCLONE_CONFIG_SECTION_NAME=$(echo "$RCLONE_DESTINATION" | sed -E 's/:.*//')
-if [[ $(grep -c "\[$RCLONE_CONFIG_SECTION_NAME\]" "$RCLONE_CONFIG_FILE") != "1" ]]; then
-  errorMsg "Could not find a config section named '$RCLONE_CONFIG_SECTION_NAME' in rclone.conf."
-  exit 1
-fi
+HOST_UID=$(ls -n /config/rclone.conf | sed -E 's/ +/ /g' | cut -d' ' -f3)
+HOST_GID=$(ls -n /config/rclone.conf | sed -E 's/ +/ /g' | cut -d' ' -f4)
 
 echo ""
-if [[ -e "$SETUP_COMPLETE_FILE" ]]; then
-  echo "Container already setup. Skipping setup."
-else
-  HOST_UID=$(ls -n /config/rclone.conf | sed -E 's/ +/ /g' | cut -d' ' -f3)
-  HOST_GID=$(ls -n /config/rclone.conf | sed -E 's/ +/ /g' | cut -d' ' -f4)
-
-  echo ""
-  echo "Setting up"
-  echo "Config:"
-  cat << EOF
-  RCLONE_DESTINATION: $RCLONE_DESTINATION
-  SHARE_USERNAME: $SHARE_USERNAME
-  SHARE_PASSWORD: $SHARE_PASSWORD
-  SHARE_NAME: $SHARE_NAME
-  HOSTNAME: $HOSTNAME
+echo "Setting up"
+echo "Config:"
+cat << EOF
+RCLONE_DESTINATION: $RCLONE_DESTINATION
+SHARE_USERNAME: $SHARE_USERNAME
+SHARE_PASSWORD: $SHARE_PASSWORD
+SHARE_NAME: $SHARE_NAME
+HOSTNAME: $HOSTNAME
 EOF
 
-  echo "Generating hostname file"
-  echo "$HOSTNAME" > /etc/hostname
+echo "Generating hostname file"
+echo "$HOSTNAME" > /etc/hostname
 
-  echo "Generating samba config"
-  cat /config-base/smb.conf | \
-    sed -E "s/HOSTNAME/$HOSTNAME/g" | \
-    sed -E "s/SHARE_NAME/$SHARE_NAME/g" | \
-    sed -E "s/SHARE_USERNAME/$SHARE_USERNAME/g" > /etc/samba/smb.conf
+echo "Generating samba config"
+cat /config-base/smb.conf | \
+  sed -E "s/HOSTNAME/$HOSTNAME/g" | \
+  sed -E "s/SHARE_NAME/$SHARE_NAME/g" | \
+  sed -E "s/SHARE_USERNAME/$SHARE_USERNAME/g" > /etc/samba/smb.conf
 
-  echo "Generating supervisord config"
-  cat /config-base/supervisord.conf | \
-    sed -E "s/SHARE_USERNAME/$SHARE_USERNAME/g" > /etc/supervisord.conf
+echo "Creating local user to match owner of rclone.conf with UID: $HOST_UID and GID: $HOST_GID."
+addgroup -g "$HOST_GID" "$SHARE_USERNAME"
+adduser -D -H -G "$SHARE_USERNAME" -s /bin/false -u "$HOST_UID" "$SHARE_USERNAME"
 
-  echo "Creating local user to match owner of rclone.conf with UID: $HOST_UID and GID: $HOST_GID."
-  addgroup -g "$HOST_GID" "$SHARE_USERNAME"
-  adduser -D -H -G "$SHARE_USERNAME" -s /bin/false -u "$HOST_UID" "$SHARE_USERNAME"
+echo "Setting samba password"
+echo -e "${SHARE_PASSWORD}\n${SHARE_PASSWORD}" | smbpasswd -a -s -c /etc/samba/smb.conf "$SHARE_USERNAME"
 
-  echo "Creating /files and setting permissions"
-  mkdir /files
-  chown -R "$HOST_UID:$HOST_GID" /files
-  chmod -R 755 /files
-
-  echo "Setting samba password"
-  echo -e "${SHARE_PASSWORD}\n${SHARE_PASSWORD}" | smbpasswd -a -s -c /etc/samba/smb.conf "$SHARE_USERNAME"
-
-  echo "1" > "$SETUP_COMPLETE_FILE"
-fi
+echo "Creating /files and setting permissions"
+mkdir -p /files
+chown -R "$HOST_UID:$HOST_GID" /files
+chmod -R 755 /files
 
 echo ""
 echo "Starting supervisord"
-export RCLONE_DESTINATION
-supervisord -c /etc/supervisord.conf
+supervisord --configuration /etc/supervisord.conf
+
+echo ""
+echo "Starting files monitor."
+/usr/local/bin/monitor_files.sh --destination "$RCLONE_DESTINATION" --unique-filenames
